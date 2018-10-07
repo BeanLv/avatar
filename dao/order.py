377b25a9f0b7f2389b289224ec2bf0dfbe83b52d
BeanLv <lb-drfto@163.com>
2018-10-08 03:06:45 +0800

    不需要openid

--- dao/order.py
+++ dao/order.py
@@ -2,6 +2,6 @@ from dao.base_dao import BaseDAO
 
 
 class OrderDAO(BaseDAO):
-    columns = ['id', 'status', 'biz', 'openid', 'realname', 'nickname', 'headimgurl', 'mobile',
+    columns = ['id', 'status', 'biz', 'realname', 'nickname', 'headimgurl', 'mobile',
                'address', 'lon', 'lat', 'installtime']
     table = "`order`"
