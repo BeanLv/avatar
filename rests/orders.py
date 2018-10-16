cbc963dca999079cedf6ee14c041b8deaae066ef
BeanLv <lb-drfto@163.com>
2018-10-16 21:43:58 +0800

    订单详情接口

--- rests/orders.py
+++ rests/orders.py
@@ -1,16 +1,23 @@
 # -*- coding: UTF-8 -*-
 
+import logging
 
 import ujson
 import flask
 
 from blueprints.rests import rests
 from exceptions import RuntimeException
+from models import UserTag
 from models.model_binder import SearchOrderModelBinder
 from dao.order_view import OrderView
 from dao.order import OrderDAO
 from dao.order_record import OrderRecordDAO
 from dao.biz import BizDAO
+from dao.operator import OperatorDAO
+from dao.qrcode import QrCodeDAO
+from services import users as userservice
+
+logger = logging.getLogger(__name__)
 
 
 @rests.route('/orders')
@@ -37,8 +44,32 @@ def orderdetail(orderid: int):
         if order is None:
             return flask.make_response(('订单不存在', 404))
 
-        order['bizname'] = BizDAO.first_or_default(id=order['biz'])['name']
+        biz = BizDAO.first_or_default(id=order['biz'])
+        if biz is None:
+            logger.warning('订单没有对应的套餐. order=%s, biz=%s', orderid, order['biz'])
+            return '订单状态异常', 412
+
+        operator = OperatorDAO.first_or_default(id=biz['operator'])
+        if operator is None:
+            logger.warning('订单没有对应的供应商. order=%s, biz=%s, operator=%d', orderid, order['biz'], biz['operator'])
+            return '订单状态异常', 412
+
+        order['bizname'] = biz['name']
+        order['opname'] = operator['name']
         order['records'] = OrderRecordDAO.all('created_at', orderid=orderid)
+
+        # 根据 source 和 userid 来判断当前用户可执行的操作，添加到返回 header 中
+        # 管理员可以派发订单也可以自己接单，二维码来源的订单可以由二维码的负责人接单
+        userid = userservice.get_context_userid()
+        ordermanagers = userservice.get_taged_usersids(tagname=UserTag.ORDERMANAGER.name)
+        order['ismanager'] = userid in ordermanagers
+
+        source = order.get('source')
+        if source:
+            qrcode = QrCodeDAO.first_or_default(id=source)
+            if not qrcode:
+                logger.warning('订单的二维码来源不存在. order=%s, source=%s', orderid, source)
+            order['isowner'] = (qrcode['owner'] == userid)
 
         return ujson.dumps(order)
 
